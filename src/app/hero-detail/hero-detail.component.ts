6cac46e076ab5b2f9f7056aaa691978116b0f45d
gdk21 <g.denysiak21@gmail.com>
2020-01-11 10:14:59 +0100

    6-HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -34,4 +34,18 @@ goBack(): void {
   this.location.back();
 }
 
+save(): void {
+  this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
+}
+
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,6 +19,20 @@ getHeroes(): void {
       .subscribe(heroes => this.heroes = heroes);
 }
 
+delete(hero: Hero): void {
+  this.heroes = this.heroes.filter(h => h !== hero);
+  this.heroService.deleteHero(hero).subscribe();
+}
+
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
+
   constructor(private heroService: HeroService) { }
 
 
